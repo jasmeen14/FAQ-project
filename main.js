9adabb8e325ddbc55a59ac6f740a1d7a6c015416
jasmeen14 <jasmeen6199@gmail.com>
2022-02-20 20:05:31 +0100

    changes in main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btns= document.querySelectorAll('question-btn');
 btns.forEach(function(btn){
 btn.addEventListener("click",function(e){ // e is event object
-    console.log(e.currentTarget.parentElement);
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
 });
 });
