ef21d31f0ab58e841e6aa4d7b5ba79565b59d79b
jasmeen14 <jasmeen6199@gmail.com>
2022-02-20 22:15:51 +0100

    changes in main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(question => {
-   // console.log(question);
-  
+      
    question.addEventListener("click",() =>{
    question.classList.toggle("active");
-    
-   })
+   
+   });
 
-   })
+   });
